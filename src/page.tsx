4d139a846c0f08cc3b06ff2da7697ae9d5aadea7
Gamer2Language <bagassatwi@gmail.com>
2025-02-27 12:38:08 +0700

    add Button component and update Header to use it

--- src/page.tsx
+++ src/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@shadcn/button";
+
 function Header() {
-  return <div className="text-3xl">nugget</div>;
+  return (
+    <div className="text-3xl">
+      <Button>Nigger utton</Button>
+    </div>
+  );
 }
 
 export default Header;
