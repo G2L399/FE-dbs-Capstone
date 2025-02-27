bd9cb181cc972a111e70c75fae7e08b8c9147ccf
Gamer2Language <bagassatwi@gmail.com>
2025-02-27 10:23:13 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+//
+const pages = import.meta.glob("./**/page.{tsx,jsx,js,ts}");
+const getRoutePath = (filePath) => {
+  const path = filePath
+    .replace("/page.tsx", "")
+    .replace("/page.jsx", "")
+    .replace("/page.js", "")
+    .replace("/page.ts", "")
+    .replace("./", "/")
+    .replace(".", "/");
+  return path;
+};
+
+const router = (route) => {
+  const newroute = route.map((item) => {
+    const NewElement = React.lazy(() => Promise.resolve(item.element()));
+    return {
+      path: item.path,
+      element: <NewElement />,
+    };
+  });
+  return createBrowserRouter(
+    createRoutesFromElements(
+      newroute.map((item, index) => {
+        return <Route key={index} path={item.path} element={item.element} />;
+      })
+    )
+  );
+};
+
+function App() {
+  const routes = Object.keys(pages).map((key) => {
+    return {
+      path: getRoutePath(key),
+      element: pages[key],
+    };
+  });
+  console.log(routes);
+
+  return (
+    <>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router(routes)} />
+      </React.Suspense>
+    </>
+  );
+}
+
+export default App;
---
+++ src/page.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div className="text-3xl">nugget</div>;
+}
+
+export default Header;
