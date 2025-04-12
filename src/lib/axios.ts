c5d4a36a0caee808e29efddce1431b6bfd8ae293
DerylFeyza <muhammadderylfeyza@gmail.com>
2025-04-13 01:16:43 +0700

    setup axios

---
+++ src/lib/axios.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
+});
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    try {
+      // const currentUser = await getCurrentUser();
+      // const { firebaseToken } = currentUser;
+      // if (firebaseToken) {
+      //   config.headers.Authorization = `Bearer ${firebaseToken}`;
+      // }
+
+      return config;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      console.error('Error response:', {
+        status: error.response.status,
+        data: error.response.data,
+        headers: error.response.headers
+      });
+    } else if (error.request) {
+      console.error('Error request:', error.request);
+    } else {
+      console.error('Error message:', error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
